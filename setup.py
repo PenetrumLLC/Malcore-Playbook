0a31932202a1bb9a12a8bf60ffe38b184b10a125
Ekultek <14183473+Ekultek@users.noreply.github.com>
2025-05-01 16:24:10 -0500

    updates

--- setup.py
+++ setup.py
@@ -8,16 +8,17 @@ if __name__ == "__main__":
         packages=find_packages(),
         version=__version__.VERSION,
         description='Malcore Playbook automates malware analysis, malware triaging, '
-                    'and analyst workflows using modular recipes and scripting',
+                    'and analyst workflows using modular recipes and DSL scripting',
         author="Thomas Perkins",
-        author_email="contact@malcore.io",
+        author_email="penetrumcorp@gmail.com",
         install_requires=[l.strip() for l in open("requirements.txt").read().splitlines()],
         long_description=open("README.md").read(),
         long_description_content_type="text/markdown",
         url="https://github.com/PenetrumLLC/malcore-playbook",
         entry_points={
             'console_scripts': [
-                'malcore-playbook=malcore_playbook.cli_tool:run'
+                'malcore-playbook=malcore_playbook.cli_tool:run',
+                'mcpb=malcore_playbook.cli_tool:run',
             ]
         }
     )
